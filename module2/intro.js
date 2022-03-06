85ef5cca4685900998467b57f2052caed394886a
KOMAL309 <komalrawat2002@gmail.com>
2022-03-06 21:18:58 +0530

    module2 updated

---
+++ module2/intro.js
@@ -0,0 +1,25 @@
+// printing in js
+console.log("swagat h aap sbhi ka");
+
+// variables in js
+let a = 10; // Number
+console.log(a);
+
+let char = 'a'; // Character
+console.log(char);
+
+let str = 'I am string'; // String
+console.log(str);
+
+let bool = true; // Boolean
+console.log(bool);
+
+for(let i=1;i<5;i++){
+    console.log(i);
+}
+
+let count = 10;
+while(count>0){
+    console.log(count);
+    count--;
+}
